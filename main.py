751d6d7655fa1c295ccc12a833681f7533afe505
Guilherme Oleano <gui_oleano@hotmail.com>
2025-06-06 23:23:33 -0300

    feat(api): add routes and Task model with database integration

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+from routes import router  # Importe o router do routes.py
+
 
 app = FastAPI()
+
+app.include_router(router) 
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    in_stock: bool = True
 
 @app.get("/")
 def read_root():
     return {"message": "Task Manager API is running!"}
+
+@app.get("/TEST")
+def say_test():
+    return {"message": "Olá, FastAPI!"}
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id, "message": f"Usuário {user_id} encontrado."}
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"message": "Item criado!", "item": item}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"message": f"Item {item_id} atualizado!", "item": item}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {"message": f"Item {item_id} deletado!"}
